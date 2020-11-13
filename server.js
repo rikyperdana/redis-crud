71a820720276fd58eed71bff18f350f863108358
Riky Perdana <perdanariky@gmail.com>
2020-11-13 19:43:30 +0700

    no bodyParser

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 var dotenv = require('dotenv').config(),
 express = require('express'),
-bodyParser = require('body-parser'),
 redis = require('redis').createClient(),
 {parse, stringify} = JSON,
 withThis = (obj, cb) => cb(obj)
 
 express()
-.use(bodyParser.json({limit: '50mb'}))
-.use(express.json())
+.use(express.json({limit: '50mb'}))
 .use(express.static('public'))
 .post('/dbCall', (req, res) => withThis(
   {
@@ -41,7 +39,15 @@ express()
       coll, ...req.body.documents.map(stringify),
       responder
     ),
-    deleteMany: () => redis.del(coll, responder)
+    deleteMany: () => redis.del(coll, responder),
+    updateMany: () => redis.lrange(
+      coll, 0, -1, (err, data) => redis.rpush(
+        coll, ...stringify(req.body.documents),
+        (err, done) => done && req.body.documents.forEach(
+          i => redis.lrem(coll, 0, i)
+        ) && res.send(stringify({data: true}))
+      )
+    )
   })[req.body.method]()
 ))
 .listen(process.env.PORT || 3000)
